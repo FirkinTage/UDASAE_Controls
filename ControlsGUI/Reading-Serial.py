208a495cda5b7c1ca8a7c4f3eaaf5649172a5d4e
FirkinTage <46443576+FirkinTage@users.noreply.github.com>
2020-10-15 16:04:23 -0400

    Update Reading-Serial.py

--- ControlsGUI/Reading-Serial.py
+++ ControlsGUI/Reading-Serial.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import serial as sr
 import numpy as np
 import pandas as pd
-import csv 
+import csv
 
 cond = False
 # a = {\
@@ -12,41 +12,49 @@ cond = False
 #     "magy":[8.95],"magz":[4.85],"habDrp":[68.95],"cdaDrp":[23.39],"watDrp":[102.65],"rssi":[-40]
 #     }
 df = pd.DataFrame()
+
+
 def get_data():
-    global cond, df
+    global cond, df,s
     if (cond == True):
-        a = s.readline()
-        a.decode()
-        df_new = pd.DataFrame.from_dict(data = a)
-        df_new.iloc[0,2] += 10
-        if (len(df) < 20):
-            df = pd.concat([df, df_new], ignore_index=True) 
+        serialString=s.readline().rstrip().decode("utf-8")     #Read data in serial buffer
+        try:
+            serialDict=eval(serialString)                               #Try to convert the raw string into a dictionary
+        except (NameError,SyntaxError,ValueError) as e:                 #catch any errors when trying to convert to dictionary
+            pass
         else:
-            df[0:19] = df[1:20]
-            df = pd.concat([df, df_new], ignore_index=True)
-            df.to_csv('Dynamic-DF.csv')
-            
-        # open file
-            with open("Dynamic-DF.csv", newline = "") as file:
-                reader = csv.reader(file)
-                # r and c tell us where to grid the labels
-                r = 1
-                for col in reader:
-                    c = 0
-                    for row in col:
-                        # i've added some styling
-                        label = tk.Label(root, width = 10, height = 2, \
-                            text = row, relief = tk.RIDGE)
-                        label.grid(row = r, column = c)
-                        c += 1
-                    r += 1
+            print(serialDict)
+            df_new = pd.DataFrame.from_dict(data=serialDict, orient="index").T
+            if (len(df) < 20):
+                df = pd.concat([df, df_new], ignore_index=True)
+            else:
+                df[0:19] = df[1:20]
+                df = pd.concat([df, df_new], ignore_index=True)
+                df.to_csv('Dynamic-DF.csv')
+
+                # open file
+                with open("Dynamic-DF.csv", newline="") as file:
+                    reader = csv.reader(file)
+                    # r and c tell us where to grid the labels
+                    r = 1
+                    for col in reader:
+                        c = 0
+                        for row in col:
+                            # i've added some styling
+                            label = tk.Label(root, width=10, height=2,
+                                             text=row, relief=tk.RIDGE)
+                            label.grid(row=r, column=c)
+                            c += 1
+                        r += 1
 
     root.after(1, get_data)
-    
+
+
 def data_start():
     global cond
     cond = True
-    #s.reset_input_buffer()
+    s.reset_input_buffer()
+
 
 def data_stop():
     global cond
@@ -57,17 +65,16 @@ def data_stop():
 root = tk.Tk()
 root.geometry('1000x1800')
 root.update()
-start = tk.Button(root,\
-     text = 'Start', command = lambda: data_start())
+start = tk.Button(root, \
+                  text='Start', command=lambda: data_start())
 start.grid()
 
 root.update()
-stop = tk.Button(root,\
-    text = 'Stop', command = lambda: data_stop())
-stop.grid(row = 0, column = 1)
+stop = tk.Button(root, \
+                 text='Stop', command=lambda: data_stop())
+stop.grid(row=0, column=1)
 
+s = sr.Serial('COM21', 9600)  # change COM Port name
+s.reset_input_buffer()
 root.after(1, get_data)
 root.mainloop()
-
-s = sr.Serial('/dev/cu.URT0', 115200) # change COM Port name
-s.reset_input_buffer()
