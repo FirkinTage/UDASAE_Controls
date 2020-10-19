c76ac5b3b5435cffbb43725ebf971712d960d49d
FirkinTage <46443576+FirkinTage@users.noreply.github.com>
2020-10-19 18:55:24 -0400

    Update ControlsGroundStation.ino

--- ControlsGroundStation/ControlsGroundStation.ino
+++ ControlsGroundStation/ControlsGroundStation.ino
@@ -9,7 +9,6 @@
 
 #include <SPI.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
 #include <RH_RF95.h>
 
  //--------LoRa Setup--------------------------------
@@ -19,8 +18,6 @@
 #define RF95_FREQ 915.0     //915MHz
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 int16_t packetNum = 0;  //packet counter
-uint8_t recvDataPacket[RH_RF95_MAX_MESSAGE_LEN];  //packet that will be received from DAS
-uint8_t recvLen = sizeof(recvDataPacket);
 
 //---------Button Setup------------------------------
 #define payBtn 5
@@ -29,14 +26,18 @@ uint8_t recvLen = sizeof(recvDataPacket);
 #define cdaDropLED 10
 int payDropState = 0,cdaDropState = 0;             //Current state of the drop buttons
 int payLEDState = LOW,cdaLEDState = LOW;           //Current state of the drop LEDs
-int payDropConfirmed = 0,cdaDropConfirmed = 0; //Drops confirmed by DAS
+int payDropConfirmed = 0,cdaDropConfirmed = 0;     //Drops confirmed by DAS
+int payBtnState, cdaBtnState;
+int lastPayBtnState = LOW, lastCdaBtnState = LOW;   //Last button state for debounce
+unsigned long debounceTime = 50;                  //Decounce delay time
+unsigned long lastPayDebounceTime = 0, lastCdaDebounceTime = 0;
 unsigned long lastMillis = 0;
 const long blinkInterval = 500;
+StaticJsonDocument<250> inData;      //Create JsonDocument object to handle incomming data     
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Wire.begin();
   Serial.println("Groundstation Initializing");
   
   //---------------LoRa Init------------------------------------------------------
@@ -65,7 +66,6 @@ void setup() {
   pinMode(cdaBtn, INPUT);
   pinMode(payDropLED, OUTPUT);
   pinMode(cdaDropLED, OUTPUT);
-  
   Serial.println("Groundstation Initialized OK");
   
 
@@ -73,67 +73,91 @@ void setup() {
 
 void loop() {
   //--------Receive any packets from DAS---------------------------------------
-  if(rf95.recv(recvDataPacket, &recvLen)){ //Message from DAS
-    Serial.println("Received Packet:");
-    char receivedPacket[recvLen + 1];
-    memcpy(receivedPacket, recvDataPacket,recvLen);
-    StaticJsonDocument<RH_RF95_MAX_MESSAGE_LEN> inData;      //Create JsonDocument object to handle incomming data     
-    StaticJsonDocument<RH_RF95_MAX_MESSAGE_LEN> outData;     //Create JsonDocument object for sending out data 
-    DeserializationError error = deserializeJson(inData, receivedPacket);
-    if(error){
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
+  if(rf95.available()){
+    uint8_t recvDataPacket[RH_RF95_MAX_MESSAGE_LEN];  //packet that will be received from DAS
+    uint8_t recvLen = sizeof(recvDataPacket);
+    if(rf95.recv(recvDataPacket, &recvLen)){ //Message from DAS
+      if(recvLen>0){
+        recvDataPacket[recvLen] = 0;
+        Serial.println("Received Packet:");
+        char* receivedPacket = (char*) &recvDataPacket;
+        //Serial.println(receivedPacket);
+        DeserializationError error = deserializeJson(inData, receivedPacket);
+        if(error){
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.c_str());
+        }
+        int dropStatus = inData["drp"];
+        if(dropStatus == 101){
+          payDropConfirmed = 1;
+        }
+        if(dropStatus == 111){
+          payDropConfirmed = 1;
+          cdaDropConfirmed = 1;
+        }
+        if(dropStatus == 10){
+          cdaDropConfirmed = 1;
+        }
+        Serial.print("{");
+        Serial.print("\"pkt\":");       serializeJson(inData["pk"], Serial);
+        Serial.print(",\"drp\":");       serializeJson(inData["dp"], Serial);
+        Serial.print(",\"alt\":");       serializeJson(inData["al"], Serial);
+        Serial.print(",\"lat\":");       serializeJson(inData["lt"], Serial);
+        Serial.print(",\"lon\":");       serializeJson(inData["ln"], Serial);
+        Serial.print(",\"hdng\":");      serializeJson(inData["hd"], Serial);
+        Serial.print(",\"spd\":");       serializeJson(inData["sp"], Serial);
+        Serial.print(",\"aclx\":");      serializeJson(inData["ax"], Serial);
+        Serial.print(",\"acly\":");      serializeJson(inData["ay"], Serial);
+        Serial.print(",\"aclz\":");      serializeJson(inData["az"], Serial);
+        Serial.print(",\"magx\":");      serializeJson(inData["mx"], Serial);
+        Serial.print(",\"magy\":");      serializeJson(inData["my"], Serial);
+        Serial.print(",\"magz\":");      serializeJson(inData["mz"], Serial);
+        Serial.print(",\"gyrox\":");     serializeJson(inData["gx"], Serial);
+        Serial.print(",\"gyroy\":");     serializeJson(inData["gy"], Serial);
+        Serial.print(",\"gyroz\":");     serializeJson(inData["gz"], Serial);
+        Serial.print(",\"habHeight\":"); serializeJson(inData["hH"], Serial);
+        Serial.print(",\"cdaHeight\":"); serializeJson(inData["cH"], Serial);
+        Serial.print(",\"watHeight\":"); serializeJson(inData["wH"], Serial);
+        Serial.print(",\"rssi\":");      Serial.print(rf95.lastRssi());
+        Serial.println("}");
+      }
     }
-    if(inData["drp"][0] == "1"){
-      payDropConfirmed = 1;
-    }
-    if(inData["drp"][1] == "1"){
-      payDropConfirmed = 1;
-    }
-    if(inData["drp"][2] == "1"){
-      cdaDropConfirmed = 1;
-    }
-    outData["pkt"]   = inData["pkt"];
-    outData["drp"]   = inData["drp"];
-    outData["alt"]   = inData["alt"];
-    outData["temp"]  = inData["temp"];
-    outData["lat"]   = inData["lat"];
-    outData["lon"]   = inData["lon"];
-    outData["hdng"]  = inData["hdng"];
-    outData["spd"]   = inData["spd"];
-    outData["aclx"]  = inData["aclx"];
-    outData["acly"]  = inData["acly"];
-    outData["aclz"]  = inData["aclz"];
-    outData["magx"]  = inData["magx"];
-    outData["magy"]  = inData["magy"];
-    outData["magz"]  = inData["magz"];
-    outData["gyrox"] = inData["gyrox"];
-    outData["gyroy"] = inData["gyroy"];
-    outData["gyroz"] = inData["gyroz"];
-    outData["hour"]  = inData["hour"];
-    outData["min"]   = inData["min"];
-    outData["sec"]   = inData["sec"];
-    outData["rssi"]  = String(rf95.lastRssi());
-    serializeJson(outData, Serial);
-    Serial.println();
   }
 
   //--------Buttons For Dropping Payloads----------------------------------------
-  int payBtnState = digitalRead(payBtn);
-  int cdaBtnState = digitalRead(cdaBtn);
-  if(payBtnState == HIGH){
-    uint8_t sendDrop[] = "PAY";
-    rf95.send(sendDrop, sizeof(sendDrop));
-    rf95.waitPacketSent();
-    payDropState = 1;
+  int payBtnReading = digitalRead(payBtn);
+  int cdaBtnReading = digitalRead(cdaBtn);
+  if(payBtnReading != lastPayBtnState){
+    lastPayDebounceTime = millis();
   }
-  if(cdaBtnState == HIGH){
-    uint8_t sendDrop[] = "CDA";
-    rf95.send(sendDrop, sizeof(sendDrop));
-    rf95.waitPacketSent();
-    cdaDropState = 1;
+  if(cdaBtnReading != lastCdaBtnState){
+    lastCdaDebounceTime = millis();
   }
-
+  if((millis() - lastPayDebounceTime) > debounceTime){
+    if(payBtnReading != payBtnState){
+      payBtnState = payBtnReading;
+    }
+    if(payBtnState == HIGH){
+      uint8_t sendDrop[] = "PAY";
+      rf95.send(sendDrop, sizeof(sendDrop));
+      rf95.waitPacketSent();
+      payDropState = 1;
+    }
+  }
+  if((millis() - lastCdaDebounceTime) > debounceTime){
+    if(cdaBtnReading != cdaBtnState){
+      cdaBtnState = cdaBtnReading;
+    }
+    if(cdaBtnState == HIGH){
+      uint8_t sendDrop[] = "CDA";
+      rf95.send(sendDrop, sizeof(sendDrop));
+      rf95.waitPacketSent();
+      cdaDropState = 1;
+    }
+  }
+  lastPayBtnState = payBtnReading;
+  lastCdaBtnState = cdaBtnReading;
+  
   //--------Blink LEDs based on drop status---------------------------------------
   unsigned long currentMillis = millis();
   //If button never pressed, turn off. If drop button pressed blink every 500 ms. If drop confirmed, stay lit.
