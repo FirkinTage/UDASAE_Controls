8042297f8a182fb893977c9a96c1af28169c95e1
FirkinTage <46443576+FirkinTage@users.noreply.github.com>
2020-10-20 13:30:53 -0400

    Update ControlsGroundStation.ino

--- ControlsGroundStation/ControlsGroundStation.ino
+++ ControlsGroundStation/ControlsGroundStation.ino
@@ -1,191 +1,253 @@
-/*
- * University of Delaware ASAE 2020-2021 Controls Team
- * Eric Rivas, Tage Firkin, Dylan Frasher, James Polemeni
- * Ground Station
- * September 19, 2020
- * 
- * TODO:
- */
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image
+from PIL import ImageTk
+import serial as sr
+import numpy as np
+import pandas as pd
+import csv
+import serial.tools.list_ports
 
-#include <SPI.h>
-#include <ArduinoJson.h>
-#include <RH_RF95.h>
+################################################### SERIAL DATA FUNCTIONS ########################################################
+# a = {\
+#     "pkt":15,"drp":101,"alt":65,"temp":84.59,"lat":49.854826,
+#     "lon":72.958413,"hdng":20.54,"spd":10.96,"aclx":2.12,"acly":6.54,
+#     "aclz":1.25,"gyrox":1.26,"gyroy":1.48,"gyroz":2.08,"magx":1.85,
+#     "magy":8.95,"magz":4.85,"habDrp":68.95,"cdaDrp":23.39,"watDrp":102.65,"rssi":-40
+#     }  # Dummy Data
+# df = pd.DataFrame.from_dict(data = a, orient = 'index').T
+cond = False
+df = pd.DataFrame()
 
- //--------LoRa Setup--------------------------------
-#define RFM95_CS 4
-#define RFM95_RST 2
-#define RFM95_INT 3
-#define RF95_FREQ 915.0     //915MHz
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
-int16_t packetNum = 0;  //packet counter
 
-//---------Button Setup------------------------------
-#define payBtn 5
-#define cdaBtn 6
-#define payDropLED 9
-#define cdaDropLED 10
-int payDropState = 0,cdaDropState = 0;             //Current state of the drop buttons
-int payLEDState = LOW,cdaLEDState = LOW;           //Current state of the drop LEDs
-int payDropConfirmed = 0,cdaDropConfirmed = 0;     //Drops confirmed by DAS
-int payBtnState, cdaBtnState;
-int lastPayBtnState = LOW, lastCdaBtnState = LOW;   //Last button state for debounce
-unsigned long debounceTime = 50;                  //Decounce delay time
-unsigned long lastPayDebounceTime = 0, lastCdaDebounceTime = 0;
-unsigned long lastMillis = 0;
-const long blinkInterval = 500;
-StaticJsonDocument<250> inData;      //Create JsonDocument object to handle incomming data     
+def get_data():
+    global cond, s, df  # , a, df
+    cda_check_drop = 0
+    wh_check_drop = 0
+    if (cond == True):
+        serialString = s.readline().rstrip().decode("utf-8")  # Read data in serial buffer
+        try:
+            serialDict = eval(serialString)  # Try to convert the raw string into a dictionary
+            # serialDict = a
+        except (NameError, SyntaxError, ValueError) as e:  # catch any errors when trying to convert to dictionary
+            pass
+        else:
+            print(serialDict)
+            df = df.append(serialDict, ignore_index=True)
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Groundstation Initializing");
-  
-  //---------------LoRa Init------------------------------------------------------
-  Serial.println("LoRa Initializing");
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed, restart system");
-    while (1);
-  }
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed, restart system");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  rf95.setTxPower(23, false);     //you can set transmitter powers from 5 to 23 dBm:
-  Serial.println("LoRa Initialized OK");
+            try:
+                altitude = format(serialDict['alt'],".2f")
+            except(NameError, SyntaxError, ValueError) as e:
+                pass
+            else:
+                str_alt.set(altitude)
 
-  //--------------Button Init------------------------------------------------------
-  pinMode(payBtn, INPUT);
-  pinMode(cdaBtn, INPUT);
-  pinMode(payDropLED, OUTPUT);
-  pinMode(cdaDropLED, OUTPUT);
-  Serial.println("Groundstation Initialized OK");
-  
+            speed = serialDict['spd']
+            str_spd = tk.StringVar()
+            str_spd.set(speed)
+            speed_num = tk.Label(alt_frame, textvariable=str_spd, font=("Fixedsys", 48), bd=5, relief="sunken")
+            speed_num.grid(row=1, column=6, sticky="ns")
+            try:
+                habCheck = float(serialDict['habHeight'])
+            except(NameError, SyntaxError, ValueError) as e:
+                pass
+            else:
+                if habCheck != 0 and wh_check_drop == 0:
+                    wh_alt = serialDict['habHeight']
+                    wh_cda_alt = tk.StringVar()
+                    wh_cda_alt.set(wh_alt)
 
-}
+                    wh_num = tk.Label(alt_frame, textvariable=wh_cda_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+                    wh_num.grid(row=1, column=4, sticky="ns")
+                    wh_check_drop = 1
 
-void loop() {
-  //--------Receive any packets from DAS---------------------------------------
-  if(rf95.available()){
-    uint8_t recvDataPacket[RH_RF95_MAX_MESSAGE_LEN];  //packet that will be received from DAS
-    uint8_t recvLen = sizeof(recvDataPacket);
-    if(rf95.recv(recvDataPacket, &recvLen)){ //Message from DAS
-      if(recvLen>0){
-        recvDataPacket[recvLen] = 0;
-        Serial.println("Received Packet:");
-        char* receivedPacket = (char*) &recvDataPacket;
-        //Serial.println(receivedPacket);
-        DeserializationError error = deserializeJson(inData, receivedPacket);
-        if(error){
-          Serial.print(F("deserializeJson() failed: "));
-          Serial.println(error.c_str());
-        }
-        int dropStatus = inData["drp"];
-        if(dropStatus == 101){
-          payDropConfirmed = 1;
-        }
-        if(dropStatus == 111){
-          payDropConfirmed = 1;
-          cdaDropConfirmed = 1;
-        }
-        if(dropStatus == 10){
-          cdaDropConfirmed = 1;
-        }
-        Serial.print("{");
-        Serial.print("\"pkt\":");       serializeJson(inData["pk"], Serial);
-        Serial.print(",\"drp\":");       serializeJson(inData["dp"], Serial);
-        Serial.print(",\"alt\":");       serializeJson(inData["al"], Serial);
-        Serial.print(",\"lat\":");       serializeJson(inData["lt"], Serial);
-        Serial.print(",\"lon\":");       serializeJson(inData["ln"], Serial);
-        Serial.print(",\"hdng\":");      serializeJson(inData["hd"], Serial);
-        Serial.print(",\"spd\":");       serializeJson(inData["sp"], Serial);
-        Serial.print(",\"aclx\":");      serializeJson(inData["ax"], Serial);
-        Serial.print(",\"acly\":");      serializeJson(inData["ay"], Serial);
-        Serial.print(",\"aclz\":");      serializeJson(inData["az"], Serial);
-        Serial.print(",\"magx\":");      serializeJson(inData["mx"], Serial);
-        Serial.print(",\"magy\":");      serializeJson(inData["my"], Serial);
-        Serial.print(",\"magz\":");      serializeJson(inData["mz"], Serial);
-        Serial.print(",\"gyrox\":");     serializeJson(inData["gx"], Serial);
-        Serial.print(",\"gyroy\":");     serializeJson(inData["gy"], Serial);
-        Serial.print(",\"gyroz\":");     serializeJson(inData["gz"], Serial);
-        Serial.print(",\"habHeight\":"); serializeJson(inData["hH"], Serial);
-        Serial.print(",\"cdaHeight\":"); serializeJson(inData["cH"], Serial);
-        Serial.print(",\"watHeight\":"); serializeJson(inData["wH"], Serial);
-        Serial.print(",\"rssi\":");      Serial.print(rf95.lastRssi());
-        Serial.println("}");
-      }
-    }
-  }
+            if float(serialDict['cdaHeight']) != 0 and cda_check_drop == 0:
+                cda_alt = serialDict['cdaHeight']
+                str_cda_alt = tk.StringVar()
+                str_cda_alt.set(cda_alt)
 
-  //--------Buttons For Dropping Payloads----------------------------------------
-  int payBtnReading = digitalRead(payBtn);
-  int cdaBtnReading = digitalRead(cdaBtn);
-  if(payBtnReading != lastPayBtnState){
-    lastPayDebounceTime = millis();
-  }
-  if(cdaBtnReading != lastCdaBtnState){
-    lastCdaDebounceTime = millis();
-  }
-  if((millis() - lastPayDebounceTime) > debounceTime){
-    if(payBtnReading != payBtnState){
-      payBtnState = payBtnReading;
-    }
-    if(payBtnState == HIGH){
-      uint8_t sendDrop[] = "PAY";
-      rf95.send(sendDrop, sizeof(sendDrop));
-      rf95.waitPacketSent();
-      payDropState = 1;
-    }
-  }
-  if((millis() - lastCdaDebounceTime) > debounceTime){
-    if(cdaBtnReading != cdaBtnState){
-      cdaBtnState = cdaBtnReading;
-    }
-    if(cdaBtnState == HIGH){
-      uint8_t sendDrop[] = "CDA";
-      rf95.send(sendDrop, sizeof(sendDrop));
-      rf95.waitPacketSent();
-      cdaDropState = 1;
-    }
-  }
-  lastPayBtnState = payBtnReading;
-  lastCdaBtnState = cdaBtnReading;
-  
-  //--------Blink LEDs based on drop status---------------------------------------
-  unsigned long currentMillis = millis();
-  //If button never pressed, turn off. If drop button pressed blink every 500 ms. If drop confirmed, stay lit.
-  if(currentMillis - lastMillis >= blinkInterval){
-    lastMillis = millis();
-    //Payload Drop LED Setting
-    if(payDropConfirmed == 1){payLEDState = HIGH;
-    }
-    else if(payDropState == 1){
-      if(payLEDState == HIGH){payLEDState = LOW;
-      }
-      else{payLEDState = HIGH;
-      }
-    }
-    else{payLEDState = LOW;
-    }
-    //CDA Drop LED Setting
-    if(cdaDropConfirmed == 1){cdaLEDState = HIGH;
-    }
-    else if(cdaDropState == 1){
-      if(cdaLEDState == HIGH){cdaLEDState = LOW;
-      }
-      else{cdaLEDState = HIGH;
-      }
-    }
-    else{cdaLEDState = LOW;
-    }
-    digitalWrite(payDropLED, payLEDState);
-    digitalWrite(cdaDropLED, cdaLEDState);
-  }
-}
+                cda_num = tk.Label(alt_frame, textvariable=str_cda_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+                cda_num.grid(row=1, column=2, sticky="ns")
+                cda_check_drop = 1
+
+            root.update_idletasks()
+    root.after(100, get_data)
+
+
+def data_stop():
+    global cond, df, second_frame
+    #cond = False
+    for col_num, col_name in enumerate(df.columns):
+        label = tk.Label(second_frame, width=6, height=1,
+                         text=col_name, relief=tk.RIDGE)
+        label.grid(row=2, column=col_num+1)
+    for i in range(len(df)):
+        for j in range(len(df.iloc[0, :])):
+            element = round(df.iloc[i, j], 2)
+            label = tk.Label(second_frame, width=6, height=1,
+                             text=element, relief=tk.RIDGE)
+            label.grid(row=i + 3, column=j+1)
+
+
+######################################################## COM PORT INPUT GUI ####################################################
+# function to return port # input
+def format_port(port_number):
+    com_port_str = 'COM' + str(port_number)
+    return (com_port_str)
+
+
+def get_port(entry):
+    print('Port selected: ', entry)
+    global comport_root
+    comport_root.destroy()
+
+    global cond
+    cond = True
+    # s.reset_input_buffer()
+
+    global com_port
+    com_port = format_port(entry)
+    return com_port
+
+
+# comport GUI
+comport_root = tk.Tk()
+comport_root.title('Please enter your COM port')
+comport_root.geometry('380x80')
+
+# entry box
+entry = tk.Entry(comport_root)
+entry.grid(row=1, column=0)
+
+# label of available COM ports
+comList = serial.tools.list_ports.comports()
+# Get list of available COM ports
+connected = ''
+for element in comList:
+    connected += (str(element.device) + '  ')
+label = tk.Label(comport_root, \
+                 text='Available COM Ports:\n' + connected,
+                 bg='white')
+label.grid(row=0, column=0)
+
+# enter button
+button = tk.Button(comport_root, \
+                   text='Enter',
+                   bg='white',
+                   command=lambda: get_port(entry.get()))
+button.grid(row=1, column=1)
+
+comport_root.mainloop()
+
+#################################################### BUILDING THE MAIN GUI ########################################################
+# creating the master window (root)
+root = tk.Tk()
+root.title('Ground Station GUI')
+# creating tab control
+tabcontrol = ttk.Notebook(root, \
+                          height=1000,
+                          width=1800)
+
+# adding tabs
+dashboard = ttk.Frame(tabcontrol)
+all_data = ttk.Frame(tabcontrol)
+tabcontrol.add(dashboard, text='Dashboard')
+tabcontrol.add(all_data, text='All DAS Data')
+tabcontrol.pack(expand=1, fill='both')
+
+# ------------------------------------------------------- DASHBOARD TAB ---------------------------------------------------------------
+# CURRENT/PAYLOAD ALTITUDES AND SPEED
+alt_frame = tk.Frame(dashboard)
+alt_frame.place(relheigh=0.165, relwidth=0.70)
+
+# live altitude
+# altitude = 0.00
+# str_alt = tk.StringVar()
+# str_alt.set(altitude)
+
+alt_label = tk.Label(alt_frame, text="Current Altitude (ft):", font=("Fixedsys", 24), fg='black')
+alt_label.grid(sticky="nesw")
+
+str_alt = tk.StringVar()
+str_alt.set("")
+alt_num = tk.Label(alt_frame, textvariable=str_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+alt_num.grid(row=1, column=0, sticky="ns")
+# alt_num = tk.Label(alt_frame, textvariable=str_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+# alt_num.grid(row = 1, sticky="ns")
+
+# CDA altitude
+# cda_alt = 0.00
+# str_cda_alt = tk.StringVar()
+# str_cda_alt.set(cda_alt)
+
+cda_label = tk.Label(alt_frame, text="Colonist drop (ft):", font=("Fixedsys", 24), fg='black')
+cda_label.grid(row=0, column=2, sticky="nesw", padx=15)
+
+# cda_num = tk.Label(alt_frame, textvariable=str_cda_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+# cda_num.grid(row=1, column = 2, sticky="ns")
+
+# Water/habitat altitude
+# wh_alt = 0.00
+# str_wh_alt = tk.StringVar()
+# str_wh_alt.set(wh_alt)
+
+wh_label = tk.Label(alt_frame, text="Water/habitat drop (ft):", font=("Fixedsys", 24), fg='black')
+wh_label.grid(row=0, column=4, sticky="nesw", padx=12)
+
+# wh_num = tk.Label(alt_frame, textvariable=str_cda_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+# wh_num.grid(row=1, column = 4, sticky="ns")
+
+# Speed
+# speed = 0.00
+# str_speed = tk.StringVar()
+# str_speed.set(speed)
+
+speed_label = tk.Label(alt_frame, text="Speed (mph):", font=("Fixedsys", 24), fg='black')
+speed_label.grid(row=0, column=6, sticky="nesw", padx=15)
+
+# speed_num = tk.Label(alt_frame, textvariable=str_cda_alt, font=("Fixedsys", 48), bd=5, relief="sunken")
+# speed_num.grid(row=1, column = 6, sticky="ns")
+
+# # GPS
+# img = Image.open("formatted_airfield_test.jpeg")
+# img = img.resize((400,480),Image.ANTIALIAS)
+# gps_img = ImageTk.PhotoImage(img)
+# gps_display = tk.Label(dashboard, image = gps_img)
+# gps_display.place(x=1000, y=250)
+
+# ----------------------------------------------------- DATA PLAYBACK TAB ---------------------------------------------------------------
+BOTH = 'both'
+LEFT = 'left'
+RIGHT = 'right'
+VERTICAL = 'vertical'
+Y = 'y'
+style = ttk.Style()
+style.configure("Vertical.TScrollbar", gripcount=0,
+                background="Green", darkcolor="DarkGreen", lightcolor="LightGreen",
+                troughcolor="gray", bordercolor="blue", arrowcolor="white")
+# making a main frame
+main_frame = tk.Frame(all_data)
+main_frame.pack(fill=BOTH, expand=1)
+# making a canvas
+data_canvas = tk.Canvas(main_frame)
+data_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+# making a scrollbar
+scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=data_canvas.yview, style="Vertical.TScrollbar")
+scrollbar.pack(side=RIGHT, fill=Y)
+# configure the canvas
+data_canvas.configure(yscrollcommand=scrollbar.set)
+data_canvas.bind('<Configure>', lambda e: data_canvas.configure(scrollregion=data_canvas.bbox('all')))
+# making a second frame for the data
+second_frame = tk.Frame(data_canvas)
+# adding that^ frame to a window to be placed in the canvas
+data_canvas.create_window((0, 300), window=second_frame, anchor='nw')
+
+# adding a display data button
+show_data_button = tk.Button(all_data, \
+                             text='Click for data', command=lambda: data_stop())
+show_data_button.place(anchor='nw')
+
+s = sr.Serial(com_port, 9600)  # change COM Port name
+s.reset_input_buffer()
+root.after(5, get_data)
+root.mainloop()
